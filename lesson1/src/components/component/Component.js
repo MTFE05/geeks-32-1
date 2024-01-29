42ab757365ffd525325e1b032c474eb7ccdb96ad
Tima <timur.moldaeliev.2005@gmail.com>
2024-01-29 19:07:53 +0600

    lesson1

---
+++ lesson1/src/components/component/Component.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import classes from './component.module.css';
+
+
+const Component = ( {text} ) => {
+
+
+    return (
+        <div className={classes.component}>
+            <h1>
+                {text}
+            </h1>
+        </div>
+    );
+};
+
+export default Component;
+
+
+
+// const user= {
+//     name: "Timur",
+//     age: 18
+// }
+//
+// const {name} = user
+//
+// console.log(name);
+
+
